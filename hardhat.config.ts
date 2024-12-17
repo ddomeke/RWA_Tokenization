9879e8ab19d4e596e3950d4455932c73fbb5989d
ddomeke <durandomeke@gmail.com>
2024-12-17 17:24:25 +0300

    hardhat.config.ts updated

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -7,14 +7,31 @@ const RPC_URL = process.env.RPC_URL || "";
 const PRIVATE_KEY = process.env.PRIVATE_KEY || "";
 
 const config: HardhatUserConfig = {
-  solidity: "0.8.24",
+  gasReporter: {
+    enabled: true,
+  },
+  solidity: {
+    version: "0.8.24",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200,
+      },
+      evmVersion: "shanghai",  // Specify the EVM version for Cancun
+    },
+  },
   defaultNetwork: "hardhat",
   networks: {
-    hardhat: {},
-    arbitrum: {
-      url: RPC_URL,
-      accounts: [PRIVATE_KEY],
+    hardhat: {
+      forking: {
+        url: process.env.RPC_URL || "",        
+        blockNumber: 278070393,
+      },
+      accounts: {
+        count: 32,
+      },
     },
+
   },
   paths: {
     sources: path.resolve(__dirname, "./contracts"), // Contracts klasörü
