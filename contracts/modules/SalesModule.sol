b828719b8b043ae9f064035d21567870c82719cf
ddomeke <durandomeke@gmail.com>
2025-01-09 13:25:22 +0300

    salesmodule updated

--- contracts/modules/SalesModule.sol
+++ contracts/modules/SalesModule.sol
@@ -1,24 +1,23 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.24;
 
-
 /**
- * @file SlaesModule.sol
- * @dev This file is part of the RWATokenization project and contains the implementation of the Sales Module.
+ * @file SalesModule.sol
+ * @dev This file is part of the RWATokenization project and contains the SalesModule contract.
  * 
  * Imports:
- * - ModularInternal.sol: Provides internal modular functionalities.
- * - IERC20.sol: Interface for the ERC20 standard token.
- * - Strings.sol: Utility library for string operations.
- * - AssetToken.sol: AssetToken contract used for tokenization of assets.
+ * - ModularInternal.sol: Provides internal modular functionality.
+ * - IERC20.sol: Interface for the ERC20 standard.
+ * - Strings.sol: Utility functions for string operations.
+ * - AssetToken.sol: AssetToken contract for tokenized assets.
  * - IRWATokenization.sol: Interface for the RWATokenization project.
  */
+
 import "../core/abstracts/ModularInternal.sol";
 import "../token/ERC20/IERC20.sol";
 import "../utils/Strings.sol";
 import {AssetToken} from "../token/AssetToken.sol";
 import {IRWATokenization} from "../interfaces/IRWATokenization.sol";
-
 
 /**
  * @title SalesModule
@@ -28,31 +27,40 @@ import {IRWATokenization} from "../interfaces/IRWATokenization.sol";
 contract SalesModule is ModularInternal {
     using AppStorage for AppStorage.Layout;
 
+    IERC20 public usdt;
+    address public owner;
+    uint256 public price; // Price of 1 token in USDT (e.g., 1 token = 1 USDT => price = 1e6 for 6 decimals)
+
     address public appAddress;
 
     uint256 private constant FEXSE_DECIMALS = 10 ** 18; // 18 decimals for FEXSE
     uint256 private constant FEXSE_PRICE_IN_USDT = 45 * 10 ** 3; // 0.045 USDT represented as 45 (scaled by 10^3)
 
-
     // Event to log profit distribution
-    event TokensSold(uint256 assetId, uint256 totalTokens, uint256 tokenPrice);
+    event TokensSold(
+        uint256 assetId,
+        address buyer,
+        uint256 totalTokens,
+        uint256 tokenPrice
+    );
     address immutable _this;
-
 
     /**
      * @dev Constructor for the SalesModule contract.
      * @param _appAddress The address of the application contract.
-     * 
+     *
      * Initializes the contract by setting the contract's own address and the application address.
      * Grants the ADMIN_ROLE to the deployer of the contract and the application address.
      */
-    constructor(address _appAddress) {
+    constructor(address _appAddress, address _usdt, uint256 _price) {
         _this = address(this);
+        usdt = IERC20(_usdt);
+        owner = msg.sender;
+        price = _price;
         appAddress = _appAddress;
         _grantRole(ADMIN_ROLE, msg.sender);
         _grantRole(ADMIN_ROLE, _appAddress);
     }
-
 
     /**
      * @notice Returns an array of FacetCut structs representing the module facets.
@@ -65,7 +73,9 @@ contract SalesModule is ModularInternal {
         bytes4[] memory selectors = new bytes4[](2);
 
         // Add function selectors to the array
-        selectors[selectorIndex++] = this.buyToken.selector;
+        selectors[selectorIndex++] = this.setPrice.selector;
+        selectors[selectorIndex++] = this.buyTokens.selector;
+        selectors[selectorIndex++] = this.buyFexse.selector;
 
         // Create a FacetCut array with a single element
         FacetCut[] memory facetCuts = new FacetCut[](1);
@@ -78,6 +88,7 @@ contract SalesModule is ModularInternal {
         });
         return facetCuts;
     }
+
 
     /**
      * @notice Allows the purchase of tokens for a specified asset.
@@ -92,7 +103,7 @@ contract SalesModule is ModularInternal {
      * @param tokenPrice The price per token.
      * @param saleCurrency The address of the currency used for the sale.
      */
-    function buyToken(
+    function buyTokens(
         uint256 assetId,
         uint256 tokenAmount,
         uint256 tokenPrice,
@@ -135,6 +146,69 @@ contract SalesModule is ModularInternal {
             ""
         );
 
-        emit TokensSold(assetId, tokenAmount, tokenPrice);
+        emit TokensSold(assetId, buyer, tokenAmount, tokenPrice);
+    }
+
+    /**
+     * @notice Allows a user to buy Fexse tokens using USDT.
+     * @dev This function is protected against reentrancy attacks using the nonReentrant modifier.
+     * @param tokenAmount The amount of Fexse tokens the user wants to buy.
+     * Requirements:
+     * - `tokenAmount` must be greater than 0.
+     * - The buyer must have an allowance of USDT for the contract that is at least equal to `usdtAmount`.
+     * - The buyer must have a USDT balance that is at least equal to `usdtAmount`.
+     * - The contract must have a balance of Fexse tokens that is at least equal to `tokenAmount`.
+     * - The transfer of USDT from the buyer to the contract must succeed.
+     * - The transfer of Fexse tokens from the contract to the buyer must succeed.
+     */
+    function buyFexse(uint256 tokenAmount) external nonReentrant {
+        AppStorage.Layout storage data = AppStorage.layout();
+
+        address buyer = msg.sender;
+        address sender = address(this);
+
+        require(tokenAmount > 0, "You must buy at least 1 token");
+
+        uint256 usdtAmount = tokenAmount * price; // Total USDT required
+
+        // Check USDT allowance and balance
+        require(
+            IERC20(usdt).allowance(buyer, address(this)) >= usdtAmount,
+            "USDT allowance too low"
+        );
+        require(
+            IERC20(usdt).balanceOf(buyer) >= usdtAmount,
+            "Insufficient USDT balance"
+        );
+
+        // Check if contract has enough tokens to sell
+        require(
+            IERC20(data.fexseToken).balanceOf(sender) >= tokenAmount,
+            "Insufficient token balance in contract"
+        );
+
+        // Transfer USDT from buyer to contract
+        require(
+            IERC20(usdt).transferFrom(buyer, sender, usdtAmount),
+            "USDT transfer failed"
+        );
+
+        // Transfer tokens from contract to buyer
+        require(
+            IERC20(data.fexseToken).transferFrom(sender, buyer, tokenAmount),
+            "Token transfer failed"
+        );
+    }
+
+    /**
+     * @notice Sets the price of the token.
+     * @dev This function can only be called by an account with the ADMIN_ROLE.
+     * It is protected against reentrancy attacks by the nonReentrant modifier.
+     * @param _price The new price of the token.
+     */
+    function setPrice(
+        uint256 _price
+    ) external nonReentrant onlyRole(ADMIN_ROLE) {
+        price = _price;
     }
 }
